280fe68fe716109181f417d4f716cb993a68fe0f
Jonas Schmitt <jonas.schmitt@fau.de>
2015-07-18 14:45:56 +0200

    Added build orders for testing

--- src/main.cpp
+++ src/main.cpp
@@ -100,18 +100,12 @@ int main(int argc, char *argv[])
     filePath2 = "./data/"+race2;
 
 
-    size_t popSize = 160 / procs;
-    size_t iterations = 10;
+    size_t popSize = 160/procs;
+    size_t iterations = 5;
     size_t genPerIt = 5;
     size_t nGoals = 2;
     size_t migrants = 2*popSize;
-    int threads = omp_get_max_threads();
-    if(argc == 5 && strncmp(argv[3], "-nthreads",9) == 0)
-    {
-        threads = std::min(threads, atoi(argv[4]));
-    }
     Vec2D minPos(0.0), maxPos(200.0,200.0);
-    omp_set_num_threads(threads);
     //cout << "Number of Threads used: " << omp_get_num_threads() << std::endl;
 
 //    MicroSimulation<Terran, Protoss> sim(minPos, maxPos, filePath1, filePath2);
