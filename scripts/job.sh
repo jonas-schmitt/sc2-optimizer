9c85fa5689c1c9f061d85295187897b2face405c
Jonas Schmitt <jonas.schmitt@fau.de>
2015-10-11 12:31:47 +0200

    Fixed cluster job script

--- scripts/job.sh
+++ scripts/job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -N sc2-optimizer
 #PBS -l nodes=4:ppn=32
-#PBS -l walltime=6:00:00
-#PBS -q normal
+#PBS -l walltime=8:00:00
+#PBS -q normal 
 #PBS -M jonas.schmitt@fau.de
 #PBS -o $PBS_JOBNAME.out -e $PBS_JOBNAME.err
 . /etc/profile.d/modules.sh
@@ -16,8 +16,6 @@ make clean
 make -j4
 cd ..
 mkdir -p ./results
-#OMP_NUM_THREADS=8 mpirun --npersocket 1 \
-#    -mca orte_num_sockets 4 -mca orte_num_cores 8 \
-#        ./build/opt lists/TerranTest.txt lists/ProtossTest.txt 50 100 50 10 > res.out
-OMP_NUM_THREADS=8 mpirun -np 16 -bysocket --bind-to-socket \
-        ./build/opt lists/TerranTest.txt lists/ProtossTest.txt 100 10 50 10 > res.out
+OMP_NUM_THREADS=8 mpirun --npersocket 1 \
+    -mca orte_num_sockets 4 -mca orte_num_cores 8 \
+        ./build/opt lists/TerranTest.txt lists/ProtossTest.txt 50 50 10 10 > test_operators.out
